238a5562a7876435e01b026ddddc8e332bacc341
Francesco Menzani <francesco@menzani.eu>
2018-07-02 20:18:55 +0200

    Implement ComponentTask counter

--- src/components/ComponentTask.java
+++ src/components/ComponentTask.java
@@ -3,6 +3,8 @@ package it.menzani.bts.components;
 import java.time.Duration;
 
 public interface ComponentTask extends ComponentElement {
+    Duration ONE_TICK = Duration.ofMillis(50);
+
     void runTask() throws IllegalArgumentException, IllegalStateException;
 
     void runTaskAsynchronously() throws IllegalArgumentException, IllegalStateException;
@@ -10,6 +12,10 @@ public interface ComponentTask extends ComponentElement {
     void runTaskLater(Duration delay) throws IllegalArgumentException, IllegalStateException;
 
     void runTaskLaterAsynchronously(Duration delay) throws IllegalArgumentException, IllegalStateException;
+
+    void runTaskTimer() throws IllegalArgumentException, IllegalStateException;
+
+    void runTaskTimerAsynchronously() throws IllegalArgumentException, IllegalStateException;
 
     void runTaskTimer(Duration period) throws IllegalArgumentException, IllegalStateException;
 
@@ -20,4 +26,30 @@ public interface ComponentTask extends ComponentElement {
     void runTaskTimerAsynchronously(Duration delay, Duration period) throws IllegalArgumentException, IllegalStateException;
 
     void cancel();
+
+    Counter newCounter(Duration period, boolean hasDelay);
+
+    static long ticks(Duration duration, String variableName) {
+        if (ONE_TICK.compareTo(duration) > 0)
+            throw new IllegalArgumentException(variableName + " must be at least " + ONE_TICK.toMillis() + " milliseconds.");
+        return duration.multipliedBy(20).toSeconds();
+    }
+
+    interface Counter {
+        boolean tick();
+
+        long getPeriod();
+
+        long getActualTicks();
+
+        default long getSkippedTicks() {
+            return getActualTicks() - 1;
+        }
+
+        long getCount();
+
+        default boolean peekNextTick() {
+            return getCount() == getPeriod() - 1;
+        }
+    }
 }
--- src/components/SimpleComponentTask.java
+++ src/components/SimpleComponentTask.java
@@ -12,6 +12,7 @@ public abstract class SimpleComponentTask extends BukkitRunnable implements Comp
     private final BornToSurvive bornToSurvive;
     private final Logger logger;
     private BukkitTask task;
+    private long period = -1;
 
     protected SimpleComponentTask(SimpleComponent component) {
         this.component = component;
@@ -39,12 +40,22 @@ public abstract class SimpleComponentTask extends BukkitRunnable implements Comp
 
     @Override
     public void runTaskLater(Duration delay) throws IllegalArgumentException, IllegalStateException {
-        task = runTaskLater(bornToSurvive, ticks(delay, "delay"));
+        task = runTaskLater(bornToSurvive, ComponentTask.ticks(delay, "delay"));
     }
 
     @Override
     public void runTaskLaterAsynchronously(Duration delay) throws IllegalArgumentException, IllegalStateException {
-        task = runTaskLaterAsynchronously(bornToSurvive, ticks(delay, "delay"));
+        task = runTaskLaterAsynchronously(bornToSurvive, ComponentTask.ticks(delay, "delay"));
+    }
+
+    @Override
+    public void runTaskTimer() throws IllegalArgumentException, IllegalStateException {
+        runTaskTimer(ONE_TICK);
+    }
+
+    @Override
+    public void runTaskTimerAsynchronously() throws IllegalArgumentException, IllegalStateException {
+        runTaskTimerAsynchronously(ONE_TICK);
     }
 
     @Override
@@ -59,27 +70,66 @@ public abstract class SimpleComponentTask extends BukkitRunnable implements Comp
 
     @Override
     public void runTaskTimer(Duration delay, Duration period) throws IllegalArgumentException, IllegalStateException {
-        task = runTaskTimer(bornToSurvive, ticks(delay, "delay"), ticks(period, "period"));
+        this.period = ComponentTask.ticks(period, "period");
+        task = runTaskTimer(bornToSurvive, ComponentTask.ticks(delay, "delay"), this.period);
     }
 
     @Override
     public void runTaskTimerAsynchronously(Duration delay, Duration period) throws IllegalArgumentException, IllegalStateException {
-        task = runTaskTimerAsynchronously(bornToSurvive, ticks(delay, "delay"), ticks(period, "period"));
-    }
-
-    private static long ticks(Duration duration, String variableName) {
-        if (duration.isNegative()) throw new IllegalArgumentException(variableName + " must be positive.");
-        return duration.toSeconds() * 20;
+        this.period = ComponentTask.ticks(period, "period");
+        task = runTaskTimerAsynchronously(bornToSurvive, ComponentTask.ticks(delay, "delay"), this.period);
     }
 
     @Override
     public void cancel() {
-        if (task == null) throw new IllegalStateException("Task was never scheduled.");
+        if (task == null) throw new IllegalStateException("Task was not scheduled.");
         task.cancel();
+    }
+
+    @Override
+    public Counter newCounter(Duration period, boolean hasDelay) {
+        return new SimpleCounter(ComponentTask.ticks(period, "period"), hasDelay);
     }
 
     @Override
     public SimpleComponent getComponent() {
         return component;
+    }
+
+    private class SimpleCounter implements Counter {
+        private final long period;
+        private long count;
+
+        private SimpleCounter(long period, boolean hasDelay) {
+            this.period = period;
+            if (!hasDelay) {
+                count = period - 1;
+            }
+        }
+
+        @Override
+        public boolean tick() {
+            if (++count == period) {
+                count = 0;
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public long getPeriod() {
+            return period;
+        }
+
+        @Override
+        public long getActualTicks() {
+            if (SimpleComponentTask.this.period == -1) throw new IllegalStateException("Task is not timer.");
+            return period / SimpleComponentTask.this.period;
+        }
+
+        @Override
+        public long getCount() {
+            return count;
+        }
     }
 }
