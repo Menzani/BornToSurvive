d8d7927d327ac4e83b55b67c7421b5a93fbb41fe
Francesco Menzani <francesco@menzani.eu>
2018-07-22 21:01:34 +0200

    [ServerView] Ignore max players

--- src/components/serverview/ServerView.java
+++ src/components/serverview/ServerView.java
@@ -4,6 +4,8 @@ import it.menzani.bts.BornToSurvive;
 import it.menzani.bts.ChatColors;
 import it.menzani.bts.components.SimpleComponent;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 
 public class ServerView extends SimpleComponent {
@@ -13,10 +15,16 @@ public class ServerView extends SimpleComponent {
 
     @EventHandler
     public void onServerListPing(ServerListPingEvent event) {
-        String motd = event.getMotd()
-                .replace("{SERVER}", getBornToSurvive().getServerName())
-                .replace("{VERSION}", getBornToSurvive().getDescription().getVersion());
-        event.setMotd(ChatColors.translate(motd));
+        String motd = event.getMotd();
+        event.setMotd(ChatColors.translate(
+                motd.replace("{SERVER}", getBornToSurvive().getServerName())
+                        .replace("{VERSION}", getBornToSurvive().getDescription().getVersion())));
         event.setMaxPlayers(Math.max(event.getNumPlayers() + 1, 50));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void ignoreMaxPlayers(AsyncPlayerPreLoginEvent event) {
+        if (event.getLoginResult() != AsyncPlayerPreLoginEvent.Result.KICK_FULL) return;
+        event.allow();
     }
 }
